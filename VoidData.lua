09fc61fd6d5c48780bda110e425091fc5fc40ef0
mzx <voidwind@qq.com>
2026-02-02 11:03:29 +0800

    Update VoidData.lua

--- VoidData.lua
+++ VoidData.lua
@@ -1,12 +1,13 @@
 function InitDatabase()
     VoidModClassicDB = VoidModClassicDB or {}
     VoidModClassicCharacterDB = VoidModClassicCharacterDB or {
-        status = {
-            PlayerInfo = true,
-            SkillLine = true,
-            ShieldInfo = true,
-            TotemInfo = true
-        },
+        status = {},
         point = {}
+    }
+    VoidModClassicCharacterDB.status = VoidModClassicCharacterDB.status or {
+        PlayerInfo = true,
+        SkillLine = true,
+        ShieldInfo = true,
+        TotemInfo = true
     }
 end
