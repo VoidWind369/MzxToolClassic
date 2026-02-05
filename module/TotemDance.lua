b1d6f97f38e9306c4b57ceb095b211dbe3ed327d
Muzixi <voidwind@qq.com>
2026-02-06 01:55:15 +0800

    Update TotemDance.lua

--- module/TotemDance.lua
+++ module/TotemDance.lua
@@ -6,7 +6,8 @@ local totem_dance = {
     },
     windfury_totem_spell_id = { 8512, 10613, 10614, 25585, 25587 },
     flametongue_totem_sprll_id = { 8227, 8249, 10526, 16387, 25557 },
-    totem_end_time = 0
+    totem_end_time = 0,
+    remaining_time = 0
 }
 
 function VoidFrame:GetGroupBuffs()
@@ -38,12 +39,16 @@ function VoidFrame:CreateTotemDanceFrame(dance_time)
         VoidModClassicCharacterDB.point.totem_dance.y)
     SetInfoFrameStyle(self.voidTotemDance)
 
-    self.totemDurationText = {
+    self.voidTotemDanceIcon = self.voidTotemDance:CreateTexture()
+    self.voidTotemDanceText = {
         self.voidTotemDance:CreateFontString(nil, "OVERLAY", "GameTooltipText"),
         self.voidTotemDance:CreateFontString(nil, "OVERLAY", "GameTooltipText")
     }
-    AddStringLeft(self.totemDurationText[1], "|cFFFF1493图腾舞|r", 1.6)
-    AddStringRight(self.totemDurationText[2], dance_time, 1.6)
+    AddIcon(self.voidTotemDanceIcon, 136114, 26, 13.5, -1.2)
+    AddStringLeft(self.voidTotemDanceText[1], "|cFFFF1493图腾舞|r", 1.6, 45)
+    AddStringRight(self.voidTotemDanceText[2], dance_time, 1.6)
+
+    GetTotemDanceGameTooltip()
 end
 
 function VoidFrame:Void_CreateTotemDance()
@@ -54,14 +59,35 @@ end
 
 function VoidFrame:Void_UpdateTotemDance()
     if self.voidTotemDance then
-        self.totemDurationText[2]:SetText(FormatRemaining())
+        if totem_dance.remaining_time > 2 then
+            VoidFrame.voidTotemDanceIcon:SetTexture(136046)
+        else
+            VoidFrame.voidTotemDanceIcon:SetTexture(136114)
+        end
+        self.voidTotemDanceText[2]:SetText(FormatRemaining())
     end
 end
 
 function FormatRemaining()
-    local remaining_time = totem_dance.totem_end_time - GetTime()
-    local est_color_str = string.format(remaining_time > 5 and "|cFFFFFF00%.1f|r" or "|cFFFF0000%.1f|r", remaining_time)
-    return remaining_time > 0 and est_color_str or "|cFFC0C0C0None|r"
+    totem_dance.remaining_time = totem_dance.totem_end_time - GetTime()
+    local est_color_str = string.format(totem_dance.remaining_time > 2 and "|cFFFFFF00%.1f|r" or "|cFFFF0000%.1f|r",
+        totem_dance.remaining_time)
+    return totem_dance.remaining_time > 0 and est_color_str or "|cFFC0C0C0--|r"
+end
+
+function GetTotemDanceGameTooltip()
+    -- 鼠标悬停事件
+    VoidFrame.voidTotemDanceIcon:SetScript("OnEnter", function(self)
+        GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+        GameTooltip:SetText("|cFFFF1493图腾舞|r")
+        GameTooltip:AddLine("用于高端萨满玩家同时保持风怒图腾与风之优雅", 1, 0.9, 0) -- RGB白色
+        GameTooltip:AddLine("作用：风怒图腾召唤后的第一次主手附魔倒计时", 0.8, 0.8, 0.8) -- 浅灰色
+        GameTooltip:Show()
+    end)
+
+    VoidFrame.voidTotemDanceIcon:SetScript("OnLeave", function()
+        GameTooltip:Hide()
+    end)
 end
 
 function MovableTotemDanceFrameStop()
